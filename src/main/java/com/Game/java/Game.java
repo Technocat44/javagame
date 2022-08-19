e18b9ec7aff9c96087567e14c7c3e5fc2b31777f
jamesaqu <jamesaqu@buffalo.edu>
2022-08-19 16:57:49 -0400

    added width and height to Game class

--- src/main/java/com/Game/java/Game.java
+++ src/main/java/com/Game/java/Game.java
@@ -2,4 +2,13 @@ package com.Game.java;
 
 public class Game {
 
+    /* public means we can access it anywhere in our project
+
+       static means for this class, if there are multiple instances
+       of the game class the width will never change.
+       static also enables us to use width in another class
+
+    * */
+    public static int width = 300;
+    public static int height = width / 16 * 9;
 }
